f17460f9f72b8d57120937772e81a0b4834c5719
Victor Rubio <vctrubio@gmail.com>
2019-03-26 13:58:26 -0600

    adding controller events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,20 @@
 class Event < ApplicationRecord
+
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+
+    validates :user, presence: true
+    validates :title, presence: true
+
+
 end
+
+# create_table "events", force: :cascade do |t|
+#   t.string "title"
+#   t.string "description"
+#   t.float "latitude"
+#   t.float "longitude"
+#   t.datetime "start_time"
+#   t.string "end_time"
+#   t.string "picture"
+#   t.bigint "user_id"
