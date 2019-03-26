27cf7819858314119e629e39ee19eb0c47958be9
Catharina Komrij <komcath@gmail.com>
2019-03-26 17:08:56 -0600

    routes set for follwing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+devise_for :users
   resources :users do
+    member do
+      get :following, :followers
+    end
     resources :events
   end
   root to: 'events#index'
