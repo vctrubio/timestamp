112a645c245da3f146b2218dba6738cd4d4431e3
Michael Villarreal <mvillarreal0605@gmail.com>
2019-03-26 18:15:36 -0600

    Created the comments controller. Haven't gotten to test it yet. Need to create User and Event views before returning to test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_event, only: [:create, :new]
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.event = @event
+    if @comment.save!
+      redirect_to user_event_path(@comment)
+    else
+      redirect_to user_event_path(@comment)
+    end
+  end
+
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :events
-  has_many :comments, through: :events
+  has_many :comments, dependent: :destroy #, through: :events
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
