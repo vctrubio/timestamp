a6048299da95a1638f81fa8e32e3ff5744c231cb
Victor Rubio <vctrubio@gmail.com>
2019-03-27 13:37:55 -0600

    errased the seeds for creating event, not neededworking on user show, creating now links to following ect.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,3 @@ puts 'Finished!'
 
 puts 'victor sucks, I am creating 10 events'
 
-10.times do
-
-event = Event.new(
-    title: 'a event',
-    description: Faker::Lorem.sentences,
-    start_time: Date.new,
-    user_id: (1..10).to_a.sample
-)
-event.save
-
-end
