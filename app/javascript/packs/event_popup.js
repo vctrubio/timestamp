e0e872ff09a622d5875e16369d3f4ae1526ec2d0
oreoorbitz <oreoorbitz@gmail.com>
2019-04-03 13:46:44 -0600

    moar fixin

--- app/javascript/packs/event_popup.js
+++ app/javascript/packs/event_popup.js
@@ -1,13 +1,11 @@
 const popUp = () => {
-
-  let markerz = document.getElementsByClassName('mapboxgl-marker')[0]
-  if (typeof markerz == 'undefined') { return false}
-  markerz.addEventListener('click', (event) => {
-  if (typeof document.getElementsByClassName('mapboxgl-popup-content')[0] !== 'undefined') {
-    let popUps = document.getElementsByClassName('mapboxgl-popup-content')[0].lastChild
-    let popUpId = popUps.dataset.id
-    let eventPopUps = document.getElementById(`${popUpId}`)
-    popUps.innerHTML = eventPopUps.innerHTML
+  document.addEventListener('click', (event) => {
+   if (typeof document.getElementsByClassName('mapboxgl-popup-content')[0] !== 'undefined') {
+  let popUps = document.getElementsByClassName('mapboxgl-popup-content')[0].lastChild
+  let popUpId = popUps.dataset.id
+  let eventPopUps = document.getElementById(`${popUpId}`)
+  console.log(eventPopUps.innerHTML)
+  popUps.innerHTML = eventPopUps.innerHTML
     }
   });
 
