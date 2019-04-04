07b31dc5e6101a2fbd5497e0760e8ae69dedc994
oreoorbitz <oreoorbitz@gmail.com>
2019-04-04 12:45:28 -0600

    omg

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,35 @@
+
+import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { autocomplete } from '../plugins/userAutocomplete';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initMapbox2 } from './user_show_map';
+import { popUp } from './event_popup';
+import {countDownTimer} from './count_down_time';
+
+initMapbox();
+initMapbox2();
+autocomplete();
+setTimeout(() => popUp(), 1000);
+countDownTimer();
+
+// geolocation JS
+const lat = document.getElementById('lat')
+const lon = document.getElementById('lon')
+const submitButton = document.getElementById('make-event')
+const eventForm = document.getElementById('new_event')
+
+const geolocationSuccess = (pos) => {
+  const latitude = pos.coords.latitude;
+  const longitude = pos.coords.longitude;
+  lat.setAttribute("value", `${latitude}`);
+  lon.setAttribute("value", `${longitude}`);
+  console.log('Coordinates', latitude, longitude);
+  eventForm.submit();
+};
+ if (!!submitButton) {
+submitButton.addEventListener("click", (event) => {
+   event.preventDefault();
+  navigator.geolocation.getCurrentPosition(geolocationSuccess);
+});
+};
--- app/javascript/packs/count_down_time.js
+++ app/javascript/packs/count_down_time.js
@@ -5,21 +5,11 @@ const countDownTimer = () => {
 
 const  object = Array.from(document.getElementsByClassName('ended-time-js'))
  object.forEach((element) => {
-   const endTime = String(element.dataset.end_time)
-   const endDate = moment(endTime)
-   const expirationString = endDate.from(moment())
-   element.innerHTML = ` ${expirationString} <i class="fas fa-stopwatch"></i> `
-
+   const endTime = element.dataset.end_time
+   const endDate = moment(String(endTime))
+   console.log(endDate)
 });
 
- const object2 = Array.from(document.getElementsByClassName('ended-time'))
-   object2.forEach((element) => {
-     const endTime = String(element.dataset.ended_time)
-     const endDate = moment(endTime)
-     const expirationString = endDate.from(moment())
-      element.innerHTML =  expirationString
-
-  });
 
 }
 
