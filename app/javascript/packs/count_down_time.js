0ae23e761c7c10fd90b2c8e5f305a8eecf02341e
oreoorbitz <oreoorbitz@gmail.com>
2019-04-04 13:35:17 -0600

    added the count down time injection in user show

--- app/javascript/packs/count_down_time.js
+++ app/javascript/packs/count_down_time.js
@@ -1,1 +1,26 @@
-// to be done later
+var moment = require('moment');
+moment().format();
+
+
+export const eventTimeLogic = () => {
+  const PlacesWhereYouWantToInjectTimeRemainingArray = Array.from(document.getElementsByClassName('ended-time-js'))
+   PlacesWhereYouWantToInjectTimeRemainingArray .forEach((element) => {
+   const endTimeUnParsed = String(element.dataset.end_time)
+   const endTimeParsed = moment(endTimeUnParsed)
+  const TimeBeforeItEndsString = endTimeParsed.from(moment())
+   element.innerHTML =  `${TimeBeforeItEndsString} <i class="fas fa-hourglass-start" style="padding-right: 1px;"></i>`
+});
+
+const PlacesWhereYouWantToInjectTimeEndedArray = Array.from(document.getElementsByClassName('ended-time'))
+  PlacesWhereYouWantToInjectTimeEndedArray  .forEach((element) => {
+   const endTimeUnParsed = String(element.dataset.ended_time)
+   const endTimeParsed = moment(endTimeUnParsed)
+  const TimeSinceEndString = endTimeParsed.from(moment())
+   element.innerHTML = ` <i class="fas fa-hourglass-end"></i> ${TimeSinceEndString}`
+   });
+
+}
+
+
+
+
